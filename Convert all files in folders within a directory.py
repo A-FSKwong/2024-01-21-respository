5262ae319a5f4da3294606148b69f77b12d615f2
A-FSKwong <157028826+A-FSKwong@users.noreply.github.com>
2024-01-22 00:39:09 +0800

    Update Convert all files in folders within a directory.py
    added code table for different types of conversions

--- Convert all files in folders within a directory.py
+++ Convert all files in folders within a directory.py
@@ -4,19 +4,25 @@ from opencc import OpenCC
 
 subprocess.check_call(["pip", "install", "opencc-python-reimplemented"])
 
+
 cc = OpenCC('s2t')  # convert from Simplified Chinese to Traditional Chinese
 
 input_dir = 'C:/Simplified'
 output_dir = 'C:/Traditional'
 
+
 # Create the output directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
+
 
 # Initialize a counter for the number of files converted
 num_files_converted = 0
 
+
 # Walk through all files in the input directory, including subdirectories
+
 for dirpath, dirnames, filenames in os.walk(input_dir):
+
     for filename in filenames:
         input_file = os.path.join(dirpath, filename)
         
@@ -35,4 +41,29 @@ for dirpath, dirnames, filenames in os.walk(input_dir):
         # Print the number of files converted so far, updating the same line each time
         print(f"\rTotal files converted: {num_files_converted}", end="")
 
+
 print("\nAll Done.")
+
+"""
+### Conversions 轉換
+
+* `hk2s`: Traditional Chinese (Hong Kong standard) to Simplified Chinese
+
+* `s2hk`: Simplified Chinese to Traditional Chinese (Hong Kong standard)
+
+* `s2t`: Simplified Chinese to Traditional Chinese
+
+* `s2tw`: Simplified Chinese to Traditional Chinese (Taiwan standard)
+
+* `s2twp`: Simplified Chinese to Traditional Chinese (Taiwan standard, with phrases)
+
+* `t2hk`: Traditional Chinese to Traditional Chinese (Hong Kong standard)
+
+* `t2s`: Traditional Chinese to Simplified Chinese
+
+* `t2tw`: Traditional Chinese to Traditional Chinese (Taiwan standard)
+
+* `tw2s`: Traditional Chinese (Taiwan standard) to Simplified Chinese
+
+* `tw2sp`: Traditional Chinese (Taiwan standard) to Simplified Chinese (with phrases)
+"""
